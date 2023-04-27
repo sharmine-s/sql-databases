0673c0af65daab42531236d2741ac451a12ea355
sharmine-s <sharmine.ss@gmail.com>
2023-04-27 14:13:22 +0100

    fixed typo

--- music_library/spec/artist_repository_spec.rb
+++ music_library/spec/artist_repository_spec.rb
@@ -4,7 +4,7 @@ require 'artist'
 
 def reset_music_library
   seed_sql = File.read('./spec/seeds_music_library.sql')
-  connection = connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
   connection.exec(seed_sql)
 end
 
